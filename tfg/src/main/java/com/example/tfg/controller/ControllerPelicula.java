77f6dd03fafd28299b3d018e5a169e1170480025
raquel99 <35260956+raquel99@users.noreply.github.com>
2019-06-01 18:24:00 +0200

    controller

--- tfg/src/main/java/com/example/tfg/controller/ControllerPelicula.java
+++ tfg/src/main/java/com/example/tfg/controller/ControllerPelicula.java
@@ -1,10 +1,41 @@
 package com.example.tfg.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tfg.model.PeliculaModel;
+import com.example.tfg.repositorios.PeliculaRepository;
+
+//Añadimos la etiqueta RestController para indicar que es un controlador de un servicio restful
+//Con la etiqueta RequestMapping indicamos el path que va a tener
 @RestController
 @RequestMapping(path="/pelicula")
 public class ControllerPelicula {
-
+	
+	//A continuacion abrimos el repositorio gracias a la etiqueta Autowired
+	@Autowired
+	PeliculaRepository peliculaRepositorio;
+	
+	//Metodo que devuelve todas las peliculas de nuestra BD
+	@GetMapping
+	@CrossOrigin
+	public @ResponseBody Iterable<PeliculaModel> allPelis(){
+		return peliculaRepositorio.findAll();
+	}
+	
+	@PostMapping
+	@CrossOrigin
+	public @ResponseBody boolean addPelis(@RequestBody PeliculaModel p){
+		if(peliculaRepositorio.findByTitulo(p.getTitulo())==null) {
+			peliculaRepositorio.save(p);
+			return true;
+		}
+		return false;
+	}
 }
--- tfg/src/main/java/com/example/tfg/repositorios/PeliculaRepository.java
+++ tfg/src/main/java/com/example/tfg/repositorios/PeliculaRepository.java
@@ -2,8 +2,10 @@ package com.example.tfg.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.tfg.model.UsuarioModel;
+import com.example.tfg.model.PeliculaModel;
 
-public interface PeliculaRepository extends CrudRepository<UsuarioModel, Integer>{
 
+public interface PeliculaRepository extends CrudRepository<PeliculaModel, Integer>{
+	
+	public PeliculaModel findByTitulo(String titulo);
 }
