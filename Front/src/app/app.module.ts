3e110ee9ad619dc80fe8833245506c567b0635d7
Diego Chipana Reyna <diego.chipana@metrica.es>
2019-06-05 16:39:10 +0200

    .

---
+++ Front/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PeliculaComponent } from './pelicula/pelicula.component';
+import {MatPaginatorModule, MatTableModule} from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeliculaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
