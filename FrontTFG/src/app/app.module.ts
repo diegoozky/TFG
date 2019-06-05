8b2a19f2b954004ad841be40786ad251b98f6d29
Raquel Sadornil Garzon <raquel.sadornil@metrica.es>
2019-06-05 10:20:59 +0200

    components

--- FrontTFG/src/app/app.module.ts
+++ FrontTFG/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PeliculaComponent } from './pelicula/pelicula.component';
+import { ActorComponent } from './actor/actor.component';
+import { DirectorComponent } from './director/director.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PeliculaComponent,
+    ActorComponent,
+    DirectorComponent
   ],
   imports: [
     BrowserModule,
